5c8fe9c91436cc5e409a98f15813a4ab099e4b26
Kiran Anand <liteninkiran@hotmail.com>
2021-09-26 16:07:09 +0100

    Adding components

---
+++ client/src/components/ChannelContainer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+    if(isCreating) {
+        return (
+            <div className="channel__container">
+                {/* Create Channel */}
+                <h1>Create Channel</h1>
+            </div>
+        )
+    }
+
+    if(isEditing) {
+        return (
+            <div className="channel__container">
+                {/* Edit Channel */}
+                <h1>Edit Channel</h1>
+            </div> 
+        )
+    }
+
+    const EmptyState = () => (
+        <div className="channel-empty__container">
+            <p className="channel-empty__first">This is the beginning of your chat history.</p>
+            <p className="channel-empty__second">Send messages, attachments, links, emojis, and more!</p>
+        </div>
+    )
+
+    return (
+        <div className=" channel__container">
+            {/* Channel */}
+            <h1>Channel</h1>
+        </div>
+    );
+}
+
+export default ChannelContainer;
